fd11b86fe311acbcaa44ba39986392a4e6eae716
bo-wang-up <56712592+bo-wang-up@users.noreply.github.com>
2024-05-12 22:05:45 +0800

    Delete CIFAR10_Recognition/demo_cifar10 directory

--- CIFAR10_Recognition/demo_cifar10/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0 python train.py -name EXP -dataset cifar10 -model spiking_resnet18 -amp -T_max 1024 -epochs 1024 -weight_decay 5e-5 -drop_rate 0.1 -T 4
-
-wait
-echo "Success!"
